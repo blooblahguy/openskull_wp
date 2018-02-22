b93abc58bf1a508e99318ef180abca901f0fa9a8
Glade Lynch <blooblahguy@gmail.com>
2018-02-22 11:57:44 -0700

    changing compilter to include sourcemap

--- css/style.php
+++ css/style.php
@@ -2,8 +2,11 @@
 	header('Content-Type: text/css');
 	require_once 'scssphp/scss.inc.php';
 	use Leafo\ScssPhp\Compiler;
+
 	$scss = new Compiler();
 	$scss->setImportPaths('');
+	$scss->setSourceMap(Compiler::SOURCE_MAP_INLINE);
+	$scss->setFormatter('Leafo\ScssPhp\Formatter\Compact');
 
 	echo $scss->compile('@import "style.scss";');
 ?>
