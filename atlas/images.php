d591a568d25f3a9a8e1d7d8f27dbb3bb9c99565a
Glade Lynch <blooblahguy@gmail.com>
2018-04-19 16:28:03 -0600

    image size change

--- atlas/images.php
+++ atlas/images.php
@@ -8,7 +8,7 @@ add_image_size( '640-thumb', 640, 640, true, array("left", "top"));
 add_image_size( 'slide-image', 1200, 600, true, array("center", "top"));
 add_image_size( 'slide-image-preview', 600, 300, true, array("center", "top"));
 
-add_image_size( 'hero-image', 1040, 500, true, array("center", "top"));
-add_image_size( 'hero-image-preview', 520, 250, true, array("center", "top"));
+add_image_size( 'hero-image', 1200, 500, true, array("center", "top"));
+add_image_size( 'hero-image-preview', 600, 250, true, array("center", "top"));
 
 ?>
